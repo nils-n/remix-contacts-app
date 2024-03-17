38ec7814199013680ac926ea1345eef50d1e9259
Nils <52067471+nils-n@users.noreply.github.com>
2024-03-17 19:36:29 +0000

    add client side rendering and handle URL params

--- app/root.tsx
+++ app/root.tsx
@@ -7,15 +7,29 @@ import {
   Scripts,
   ScrollRestoration,
   Outlet,
+  Link,
+  useLoaderData,
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css?url";
+
+import { json } from "@remix-run/node";
+
+// existing contacts
+import { getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+// add loader
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -43,14 +57,28 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
