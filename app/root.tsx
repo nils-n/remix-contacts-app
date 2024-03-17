4ef74219290d288af4f7ddefe45f33ca968efd43
Nils <52067471+nils-n@users.noreply.github.com>
2024-03-17 19:12:58 +0000

    render contact card

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Meta,
   Scripts,
   ScrollRestoration,
+  Outlet,
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css?url";
@@ -51,6 +52,9 @@ export default function App() {
               </li>
             </ul>
           </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   const contact = {
     first: "Your",
     last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
+    avatar: "https://unsplash.it/200",
     twitter: "your_handle",
     notes: "Some notes",
     favorite: true,
